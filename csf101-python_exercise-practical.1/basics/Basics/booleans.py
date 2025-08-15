3c3cf04dbce510c30f8f3b73c7da530b104863bb
deedox605 <deedox605>
2025-08-15 12:04:38 +0600

    first commit

---
+++ csf101-python_exercise-practical.1/basics/Basics/booleans.py
@@ -0,0 +1,7 @@
+is_student = True
+is_unemployed = False
+print(is_student, is_unemployed)
+is_student_and_employed = is_student
+print(is_student_and_employed)
+is_student_or_employed = is_student
+print(is_student_or_employed)
---
+++ csf101-python_exercise-practical.1/control_structure/conditionals.py
@@ -0,0 +1,6 @@
+number = 10
+if number > 0:
+    print("the number is positive.")
+else:
+    print("the number is non-positive.")
+    
